b3c87e0b6592223f52fb17b3d069e63d192e6cdb
gizmo385 <gizmo385@users.noreply.github.com>
2025-12-22 22:19:07 -0700

    Add postgres

---
+++ postgres/run_backups.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+. .backup_env
+
+SUCCESS_WEBHOOK="$BACKUP_STATUS_UPTIME_MONITOR?status=up&msg=Success"
+FAILURE_WEBHOOK="$BACKUP_STATUS_UPTIME_MONITOR?status=down&msg=Failure"
+
+echo "Generating postgres database dump @ $BACKUP_FILE"
+if sudo docker exec -t $CONTAINER_NAME pg_dumpall -U $BACKUP_USER > $BACKUP_FILE; then
+	echo "Finished generating backup, scanning for old backups"
+	curl $SUCCESS_WEBHOOK
+	find $BACKUP_DIR -maxdepth 1 -mtime +$DAYS_TO_KEEP -name "*-daily" -exec rm -rf '{}' ';'
+else
+	echo "FAILED TO RUN THE BACKUP"
+	curl $FAILURE_WEBHOOK
+fi
